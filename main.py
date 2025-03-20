c5258e2bb5c066f05bbe0ff9073a3d33320887fd
Ryan_Reboot <rfgwest@gmail.com>
2025-03-19 20:59:18 -0700

    E: forward & back movement and trying to add groups

--- main.py
+++ main.py
@@ -15,14 +15,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for object in drawable:
+            drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
